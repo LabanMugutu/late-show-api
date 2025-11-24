e59c61211cd919a3d3f688b7efed04f2d93219f5
LabanMugutu <labanmugutu@gmail.com>
2025-11-24 08:31:15 +0300

    Begins working on conftest.py

--- server/seed.py
+++ server/seed.py
@@ -7,6 +7,7 @@ This will create (or replace) `app.db` in project root.
 """
 from app import create_app
 from models import db, Episode, Guest, Appearance
+
 app = create_app()
 
 with app.app_context():
@@ -23,3 +24,16 @@ with app.app_context():
     g1 = Guest(name="Michael J. Fox", occupation="actor")
     g2 = Guest(name="Sandra Bernhard", occupation="Comedian")
     g3 = Guest(name="Tracey Ullman", occupation="television actress")
+
+    db.session.add_all([e1, e2, e3, g1, g2, g3])
+    db.session.commit()
+
+    # Appearances
+    a1 = Appearance(rating=4, episode=e1, guest=g1)
+    a2 = Appearance(rating=5, episode=e2, guest=g3)
+    a3 = Appearance(rating=3, episode=e1, guest=g2)
+
+    db.session.add_all([a1, a2, a3])
+    db.session.commit()
+
+    print("Seeded database successfully.")

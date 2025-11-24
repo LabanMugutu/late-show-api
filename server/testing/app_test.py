348d6a42c8ef36f5a5fdd7956ac894ddd13dac57
LabanMugutu <labanmugutu@gmail.com>
2025-11-24 08:43:35 +0300

    Includes get episodes test

--- server/testing/app_test.py
+++ server/testing/app_test.py
@@ -3,3 +3,9 @@ Integration tests for endpoints ensuring correct JSON shape & HTTP statuses.
 """
 import json
 from models import Episode, Guest, Appearance, db
+def test_get_episodes(client):
+    resp = client.get("/episodes")
+    assert resp.status_code == 200
+    data = resp.get_json()
+    assert isinstance(data, list)
+    assert 'id' in data[0] and 'date' in data[0] and 'number' in data[0]
